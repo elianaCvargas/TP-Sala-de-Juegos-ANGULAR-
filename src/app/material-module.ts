d369ba93405556de285a7121952f9643559aee30
Proyectos UTN <carla.e.vargas@gmail.com>
2020-09-20 01:41:56 -0300

    corrijo error

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -42,7 +42,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatTreeModule} from '@angular/material/tree';
 import {OverlayModule} from '@angular/cdk/overlay';
-import {OverlayModule} from '@angular/material/';
+// import {OverlayModule} from '@angular/material/';
 
 @NgModule({
   exports: [
