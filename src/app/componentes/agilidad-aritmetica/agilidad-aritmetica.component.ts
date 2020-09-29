85d15f7866b6b9219e8c6a8728071b6783ac420f
Proyectos UTN <carla.e.vargas@gmail.com>
2020-09-29 01:06:28 -0300

    agilidad-aritmetica

--- src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
+++ src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
@@ -8,21 +8,27 @@ import {Subscription} from "rxjs";
   styleUrls: ['./agilidad-aritmetica.component.scss']
 })
 export class AgilidadAritmeticaComponent implements OnInit {
-   @Output()
-  enviarJuego :EventEmitter<any>= new EventEmitter<any>();
+   @Output() enviarJuego :EventEmitter<any>= new EventEmitter<any>();
+
   nuevoJuego : JuegoAgilidad;
+  Mensajes: string;
+  operacionArtimeticaGenerada: boolean;
+
   ocultarVerificar: boolean;
   Tiempo: number;
   repetidor:any;
+
   private subscription: Subscription;
-  ngOnInit() {
-  }
-   constructor() {
-     this.ocultarVerificar=true;
-     this.Tiempo=5;
-    this.nuevoJuego = new JuegoAgilidad();
+
+  constructor() {
+    this.ocultarVerificar=false;
+    this.Tiempo=5;
+    this.nuevoJuego = new JuegoAgilidad('Agilidad Aritmetica', false, 'username');
     console.info("Inicio agilidad");
   }
+
+  ngOnInit() {}
+
   NuevoJuego() {
     this.ocultarVerificar=false;
    this.repetidor = setInterval(()=>{
@@ -36,15 +42,49 @@ export class AgilidadAritmeticaComponent implements OnInit {
         this.Tiempo=5;
       }
       }, 900);
-
   }
+
+  generarOperacion() {
+    this.nuevoJuego.generarOperacion();
+    this.operacionArtimeticaGenerada = true;
+  }
+
   verificar()
   {
     this.ocultarVerificar=false;
     clearInterval(this.repetidor);
+    console.info('agilidad aritmetica:', this.nuevoJuego.gano);
 
+    if (this.nuevoJuego.verificar()) {
+      console.log('antes de enviar');
+      this.enviarJuego.emit(this.nuevoJuego);
+      this.MostarMensaje('Sos un Genio!!!', true);
 
+    } else {
+      let mensaje: string;
+      mensaje = 'Lo siento, necesitas mejorar tu abilidad con la caluculadora master';
+    }
 
+    this.nuevoJuego.resultado = 0;
+    this.ocultarVerificar=false;
   }
 
+  MostarMensaje (
+    mensaje: string = 'este es el mensaje',
+    ganador: boolean = false
+  ) {
+    this.Mensajes = mensaje;
+    var x = document.getElementById('snackbar');
+    if (ganador) {
+      x.className = 'show Ganador';
+    } else {
+      x.className = 'show Perdedor';
+    }
+    var modelo = this;
+    setTimeout(function () {
+      x.className = x.className.replace('show', '');
+      modelo.ocultarVerificar = false;
+    }, 3000);
+    console.info('objeto', x);
+  }
 }
